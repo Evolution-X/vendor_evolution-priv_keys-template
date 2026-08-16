fed6526d27440d6c1317b465ea399a287313ba19
LuK1337 <priv.luk@gmail.com>
2026-08-16 16:50:31 +0200

    lineage-priv-template: Check apexes in `*/apex` instead of `obj`
    Change-Id: Ief3316f0dc809738feba692d0a581d188c20eaf5

--- check_keys.py
+++ check_keys.py
@@ -52,8 +52,8 @@ def main():
             check_public_key,
             (
                 glob.glob(f"{out}/obj/**/*.apk", recursive=True)
-                + glob.glob(f"{out}/obj/**/*.apex", recursive=True)
-                + glob.glob(f"{out}/obj/**/*.capex", recursive=True)
+                + glob.glob(f"{out}/*/apex/*.apex", recursive=True)
+                + glob.glob(f"{out}/*/apex/*.capex", recursive=True)
             ),
         )
 
